5563d420d9d34427ae8cb0b75893b38dfec70614
lisagjh <lisagjhoorn@outlook.com>
2025-07-09 15:51:54 +0200

    remove skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,9 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-
 import Count from './components/Count.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
     <Count />
-
-    <TheWelcome />
   </main>
 </template>

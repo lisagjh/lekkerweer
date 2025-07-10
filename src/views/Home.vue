41b8e0feacf84d6a9e5e98d673505757d38b0246
lisagjh <lisagjhoorn@outlook.com>
2025-07-10 10:49:02 +0200

    great succes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,45 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const city = ref('Amsterdam')
+const weather = ref(null)
+const loading = ref(true)
+
+console.log('API Key:', import.meta.env.VITE_API_KEY)
+
+const KEY = import.meta.env.VITE_API_KEY
+
+const fetchWeather = async () => {
+  loading.value = true
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=metric&appid=${KEY}`,
+    )
+    const data = await response.json()
+    weather.value = data
+  } catch (err) {
+    console.error('Fout bij ophalen weer:', err)
+  } finally {
+    loading.value = false
+  }
+}
+
+onMounted(fetchWeather)
+</script>
+
 <template>
-    <h1>HOME</h1>
+  <main>
+    <h1>Weer in {{ city }}</h1>
+
+    <div v-if="loading">Laden...</div>
+
+    <div v-else-if="weather?.weather && weather?.main">
+      <p>{{ weather.weather[0].description }}</p>
+      <p>Temp: {{ weather.main.temp }}°C</p>
+    </div>
+
+    <div v-else>
+      <p>Fout bij ophalen van weerdata 😓</p>
+    </div>
+  </main>
 </template>
